2e283c7a8c416de83c0db11b86b6f81efd7f37b4
David Yap <davvyap@gmail.com>
2023-02-14 14:44:09 +0800

    3rd part of task02

--- task02/src/myapp/Main.java
+++ task02/src/myapp/Main.java
@@ -26,20 +26,19 @@ public class Main {
 
             // testing for 1st text file
             String path1 = dirList.get(0);
-            List<String> textData1 = getDataFromText(path1);
-            for (String string : textData1) {
-                System.out.println(string);
-            }
 
-            List<String> wordTextData1 = getWordsFromPhrase(path1);
-            for (String string : wordTextData1) {
-                System.out.print(string + " > ");
-            }
+            NextWord doc1 = new NextWord(path1);
+            //System.out.println(doc1.getDataFromText());
+            List<String> wordTextData1 = doc1.getWordsFromPhrase();
+            List<String> list1 = doc1.getUniqueList();
+            //System.out.println(list1);
 
-            Map<String,Integer> mapTextData1 = getWordCount(path1);
-            mapTextData1.forEach((k,v) -> System.out.println(k + " >>> " + v));
+            Map<String,Map<String,Integer>> map1 = doc1.getFinalMap();
+            Map<String,Map<String,Double>> finalMap1 = doc1.getSolutionMap(map1);
+            doc1.print(finalMap1);
 
-            Map<String,Integer> testingMap = getNextWordFreqCount(path1, "hands");
+
+           
         }
     }
 
@@ -62,120 +61,7 @@ public class Main {
         return list;
     }
 
-    public static List<String> getDataFromText(String path) throws IOException{
-        
-        BufferedReader br = new BufferedReader(new FileReader(path));
-
-        List<String> list = new LinkedList<>();
-
-        String line;
-
-        while ((line = br.readLine()) != null){
-            list.add(line);
-        }
-        br.close();
-        return list;
-    }
-
-    public static List<String> getWordsFromPhrase(String path) throws IOException{
-        
-        String[] punctuations = {".", ",", ":", "!", "-", "(", ")", "\'", "\"", ";"}; 
-        List<String> rawList = getDataFromText(path);
-        List<String> wordList = new LinkedList<>();
-
-        for (String line : rawList) {
-            String newLine = line.trim().replaceAll("\\p{Punct}", " ");
-            String[] tempArray = newLine.split(" ");
-            for (String word : tempArray) {
-                wordList.add(word.toLowerCase());
-            }
-        }
-        return wordList;
-    }
-
-    // to get count of each word
-    public static Map<String,Integer> getWordCount(String path) throws IOException{
-        List<String> words = getWordsFromPhrase(path);
-        Map<String,Integer> wordsMap = new HashMap<>();
-
-        for (String word : words) {
-            if(wordsMap.containsKey(word)){
-                wordsMap.put(word, wordsMap.get(word)+1);
-            }else{
-                wordsMap.put(word, 1);
-            }
-        }
-        return wordsMap;
-    }
-
-    // to get the frequency of next word for each word
-    public static Map<String,Map<String,Integer>> getFreqCountTest(String path) throws IOException{
-        Map<String,Map<String,Integer>> nextWordFreq = new HashMap<>();
-
-        // to get the unique key for each word
-        Map<String,Integer> map = getWordCount(path);
-        Set<Entry<String,Integer>> set = map.entrySet();
-        List<Entry<String,Integer>> list = new ArrayList<>(set);
-
-        // list to store unique key from the txt file
-        List<String> words = new ArrayList<>();
-
-        for (Entry<String,Integer> entry : list) {
-            words.add(entry.getKey());
-        }
-
-        // print to check
-        // for (String string : words) {
-        //     System.out.println("Keys : " + string);
-        // }
-
-        // get all words (not unique) from txt file
-        List<String> allWords = getWordsFromPhrase(path);
-
-        for (int i = 0; i < allWords.size()-1; i++) {   // word
-            for (int j = 1; j < allWords.size(); j++) {     // next word
-                if(nextWordFreq.containsKey(allWords.get(i)) && nextWordFreq.get(allWords.get(i)).containsKey(allWords.get(j))){
- 
-                }
-            }
-        }
-        
-        return nextWordFreq;
-    }
-
-    // to get the frequency of next word for each word
-    public static Map<String,Integer> getNextWordFreqCount(String path, String word) throws IOException{
-        Map<String,Integer> nextWordFreqMap = new HashMap<>();
-
-        // get all words (not unique) from txt file
-        List<String> allWords = getWordsFromPhrase(path);
-
-        List<Integer> nextWordIndex = new ArrayList<>();
-
-        System.out.println("Word selected >>> " + word);
-        System.out.println("Size of list >>> " + allWords.size());
-        
-        int index = allWords.indexOf(word);
-        System.out.println("Index of word selected >>> " + index);
-        System.out.println(">>> " + allWords.get(index));
-
-        Map<String,Integer> map = getWordCount(path);
-        int count = map.get(word);
-        System.out.println("Count of word selected >>> " + count);
-
-        // use iterator to iterate the list and get the index
-        Iterator<String> iterator = allWords.iterator();
-        int i = 0;
-        while(iterator.hasNext()){
-            String item = iterator.next();
-            if(word.equals(item)){
-                nextWordIndex.add(i);
-            }
-            i++;
-        }
-        System.out.println("last index >>> " + allWords.lastIndexOf(word));
-        System.out.println(nextWordIndex);
-
-        return nextWordFreqMap;
-    }
+    
 }
+
+
---
+++ task02/src/myapp/NextWord.java
@@ -0,0 +1,173 @@
+package myapp;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class NextWord {
+    private String path;
+    private List<String> allWordsList = new ArrayList<>();
+    private List<String> uniqueList = new ArrayList<>();
+
+    //constructor
+    public NextWord(String path) throws IOException {
+        this.path = path;
+        // this.allWordsList = Main.getWordsFromPhrase(path);
+    }
+
+
+    public List<String> getDataFromText() throws IOException{
+        
+        BufferedReader br = new BufferedReader(new FileReader(path));
+
+        List<String> list = new LinkedList<>();
+
+        String line;
+
+        while ((line = br.readLine()) != null){
+            list.add(line);
+        }
+        br.close();
+        return list;
+    }
+
+    public List<String> getWordsFromPhrase() throws IOException{
+        List<String> rawList = getDataFromText();
+        List<String> wordList = new LinkedList<>();
+
+        for (String line : rawList) {
+            String newLine = line.trim().replaceAll("\\p{Punct}", " ");
+            String[] tempArray = newLine.split(" ");
+            for (String word : tempArray) {
+                wordList.add(word.toLowerCase());
+            }
+        }
+        allWordsList = wordList;
+        return wordList;
+    }
+
+    public List<String> getUniqueList() throws IOException{
+        List<String> uniqueWords = new ArrayList<>();
+        for (String word : allWordsList) {
+            if(!uniqueWords.contains(word)){
+                uniqueWords.add(word);
+            }
+        }
+        uniqueList = uniqueWords;
+        return uniqueWords;
+    }
+
+    // to get count of each word
+    public Map<String,Integer> getWordCount() throws IOException{
+        List<String> words = getWordsFromPhrase();
+        Map<String,Integer> wordsMap = new HashMap<>();
+
+        for (String word : words) {
+            if(wordsMap.containsKey(word)){
+                wordsMap.put(word, wordsMap.get(word)+1);
+            }else{
+                wordsMap.put(word, 1);
+            }
+        }
+        return wordsMap;
+    }
+
+    // to get the frequency of next word for each word
+    public Map<String,Integer> getNextWordFreqCount(String word) throws IOException{
+        // get all words (not unique) from txt file
+        List<String> allWords = getWordsFromPhrase();
+
+        List<Integer> nextWordIndex = new ArrayList<>();
+        List<String> nextWordList = new ArrayList<>();
+
+        //System.out.println("Word selected >>> " + word);
+        
+        int index = allWords.indexOf(word);
+        // System.out.println("Index of word selected >>> " + index);
+        // System.out.println(">>> " + allWords.get(index));
+
+        Map<String,Integer> map = getWordCount();
+        int count = map.get(word);
+        // System.out.println("Count of word selected >>> " + count);
+
+        // use iterator to iterate the list and get the index
+        Iterator<String> iterator = allWords.iterator();
+        int i = 0;
+        while(iterator.hasNext() && i < allWords.size()-1){
+            String item = iterator.next();
+            if(word.equals(item)){
+                nextWordIndex.add(i+1);
+            }
+            i++;
+        }
+        //System.out.println("last index >>> " + allWords.lastIndexOf(word));
+        //System.out.println(nextWordIndex);
+
+        for (int position : nextWordIndex) {
+            nextWordList.add(allWords.get(position));
+        }
+        // System.out.println(nextWordList);
+
+        // put the next words in a map
+        Map<String,Integer> nextWordFreqMap = new HashMap<>();
+        for (String string : nextWordList) {
+            if(nextWordFreqMap.containsKey(string)){
+                nextWordFreqMap.put(string,nextWordFreqMap.get(string)+1);
+            }else{
+                nextWordFreqMap.put(string,1);
+            }
+        }
+        return nextWordFreqMap;
+    }
+
+
+    public Map<String,Map<String,Integer>> getFinalMap() throws IOException{
+        Map<String,Map<String,Integer>> finalMap = new HashMap<>();
+        
+        for (String string : uniqueList) {
+            finalMap.put(string, getNextWordFreqCount(string));
+        }
+        return finalMap;
+    }
+
+    public Map<String,Map<String,Double>> getSolutionMap(Map<String,Map<String,Integer>> map){
+        Map<String,Map<String,Double>> answerMap = new HashMap<>();
+    
+        //calculate the probability
+        for(Map.Entry<String, Map<String,Integer>> t :map.entrySet()){
+            String key = t.getKey();
+
+            Set<Entry<String,Integer>> set = t.getValue().entrySet();
+            List<Entry<String,Integer>> tempList = new ArrayList<>(set);
+            
+            //tempList.forEach(e -> System.out.println(e));
+            // for each key, put the key.entrySet() after the division of total
+            double total = 0;
+            Map<String,Double> tempMap = new HashMap<>();
+            for (Entry<String,Integer> entry : tempList) {
+                System.out.println("value >>> " + entry.getValue());
+                total += entry.getValue();
+                tempMap.put(entry.getKey(), entry.getValue()/total);
+            }
+            answerMap.put(key, tempMap);
+        }
+        return answerMap;
+    }
+
+    public void print(Map<String,Map<String,Double>> map){
+        for(Map.Entry<String, Map<String,Double>> t :map.entrySet()){
+            String key = t.getKey();
+            System.out.println(key + "\n");
+          for (Map.Entry<String,Double> e : t.getValue().entrySet())
+            System.out.println("\t" + e.getKey()+ " " +e.getValue());
+        }
+     }
+}
